96c91b04ea697aca44cb11301a6fa48a94447bec
Aram Hamo <contact@aramhamo.me>
2023-08-03 02:04:14 +0200

    modified:   controllers/Home.php

--- controllers/Home.php
+++ controllers/Home.php
@@ -1,9 +1,12 @@
 <?php declare(strict_types=1);
 namespace AramHamo\Mvc\Controllers;
 use AramHamo\MvcCore\View;
+use AramHamo\MvcCore\Middleware;
 
-class Home{
+class Home extends Middleware{
   public function get(){
+    $m = new Middleware;
+    $m->middleware("auth");
     return View::render("home",array("title"=>"home"));
   }
   public function post(){
